d3a73f3f33140c0a31e8252d838ddbad9a3452db
Israrullah <70327713+IsrarullahZaheer@users.noreply.github.com>
2024-04-02 11:06:44 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -5,10 +5,10 @@ const TargetType = require('../../extension-support/target-type');
 class Scratch3YourExtension {
 
     constructor (runtime) {
-      import('syllable')
-      .then((syllableModule) => {
-        this.syllable = syllableModule.syllable;
-      });
+      // import('syllable')
+      // .then((syllableModule) => {
+      //   this.syllable = syllableModule.syllable;
+      // });
         // put any setup for your extension here
     }
 
@@ -18,10 +18,10 @@ class Scratch3YourExtension {
     getInfo () {
         return {
             // unique ID for your extension
-            id: 'yourScratchExtension',
+            id: 'yourScratchExtension1',
 
             // name that will be displayed in the Scratch UI
-            name: 'Helloworld Extension demo',
+            name: 'Helloworld Extension from Pre-Project',
 
             // colours to use for your extension blocks
             color1: '#000099',
@@ -33,59 +33,59 @@ class Scratch3YourExtension {
 
             // your Scratch blocks
             blocks: [
-                {
-                    // name of the function where your block code lives
-                    opcode: 'myFirstBlock',
+                // {
+                //     // name of the function where your block code lives
+                //     opcode: 'myFirstBlock',
 
-                    // type of block - choose from:
-                    //   BlockType.REPORTER - returns a value, like "direction"
-                    //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
-                    //   BlockType.COMMAND - a normal command block, like "move {} steps"
-                    //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
-                    blockType: BlockType.REPORTER,
+                //     // type of block - choose from:
+                //     //   BlockType.REPORTER - returns a value, like "direction"
+                //     //   BlockType.BOOLEAN - same as REPORTER but returns a true/false value
+                //     //   BlockType.COMMAND - a normal command block, like "move {} steps"
+                //     //   BlockType.HAT - starts a stack if its value changes from false to true ("edge triggered")
+                //     blockType: BlockType.REPORTER,
 
-                    // label to display on the block
-                    text: 'Title for ISBN book [BOOK_NUMBER]',
+                //     // label to display on the block
+                //     text: 'Title for ISBN book [BOOK_NUMBER]',
 
-                    // true if this block should end a stack
-                   // terminal: false,
+                //     // true if this block should end a stack
+                //    // terminal: false,
 
-                    // where this block should be available for code - choose from:
-                    //   TargetType.SPRITE - for code in sprites
-                    //   TargetType.STAGE  - for code on the stage / backdrop
-                    // remove one of these if this block doesn't apply to both
-                    // filter: [ TargetType.SPRITE, TargetType.STAGE ],
+                //     // where this block should be available for code - choose from:
+                //     //   TargetType.SPRITE - for code in sprites
+                //     //   TargetType.STAGE  - for code on the stage / backdrop
+                //     // remove one of these if this block doesn't apply to both
+                //     // filter: [ TargetType.SPRITE, TargetType.STAGE ],
 
-                    // arguments used in the block
-                    arguments: {
-                        BOOK_NUMBER: {
-                            // default value before the user sets something
-                            defaultValue: 1718500564,
+                //     // arguments used in the block
+                //     arguments: {
+                //         BOOK_NUMBER: {
+                //             // default value before the user sets something
+                //             defaultValue: 1718500564,
 
-                            // type/shape of the parameter - choose from:
-                            //     ArgumentType.ANGLE - numeric value with an angle picker
-                            //     ArgumentType.BOOLEAN - true/false value
-                            //     ArgumentType.COLOR - numeric value with a colour picker
-                            //     ArgumentType.NUMBER - numeric value
-                            //     ArgumentType.STRING - text value
-                            //     ArgumentType.NOTE - midi music value with a piano picker
-                            type: ArgumentType.NUMBER
-                        },
-                        // MY_STRING: {
-                        //     // default value before the user sets something
-                        //     defaultValue: 'helloworld!',
+                //             // type/shape of the parameter - choose from:
+                //             //     ArgumentType.ANGLE - numeric value with an angle picker
+                //             //     ArgumentType.BOOLEAN - true/false value
+                //             //     ArgumentType.COLOR - numeric value with a colour picker
+                //             //     ArgumentType.NUMBER - numeric value
+                //             //     ArgumentType.STRING - text value
+                //             //     ArgumentType.NOTE - midi music value with a piano picker
+                //             type: ArgumentType.NUMBER
+                //         },
+                //         // MY_STRING: {
+                //         //     // default value before the user sets something
+                //         //     defaultValue: 'helloworld!',
 
-                        //     // type/shape of the parameter - choose from:
-                        //     //     ArgumentType.ANGLE - numeric value with an angle picker
-                        //     //     ArgumentType.BOOLEAN - true/false value
-                        //     //     ArgumentType.COLOR - numeric value with a colour picker
-                        //     //     ArgumentType.NUMBER - numeric value
-                        //     //     ArgumentType.STRING - text value
-                        //     //     ArgumentType.NOTE - midi music value with a piano picker
-                        //     type: ArgumentType.STRING
-                        // }
-                    }
-                },
+                //         //     // type/shape of the parameter - choose from:
+                //         //     //     ArgumentType.ANGLE - numeric value with an angle picker
+                //         //     //     ArgumentType.BOOLEAN - true/false value
+                //         //     //     ArgumentType.COLOR - numeric value with a colour picker
+                //         //     //     ArgumentType.NUMBER - numeric value
+                //         //     //     ArgumentType.STRING - text value
+                //         //     //     ArgumentType.NOTE - midi music value with a piano picker
+                //         //     type: ArgumentType.STRING
+                //         // }
+                //     }
+                // },
                 {
                   // function where your code logic lives
                   opcode: 'mySecondBlock',
@@ -94,7 +94,7 @@ class Scratch3YourExtension {
                   blockType: BlockType.REPORTER,
           
                   // label to display on the block
-                  text: 'Syllables in [MY_TEXT]',
+                  text: 'Pre-Project [MY_TEXT]',
           
                   // arguments used in the block
                   arguments: {
@@ -116,23 +116,23 @@ class Scratch3YourExtension {
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    myFirstBlock ({ BOOK_NUMBER }) {
-        return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            }
-            else {
-              return { title: 'Unknown' };
-            }
-          })
-          .then((bookinfo) => {
-            return bookinfo.title;
-          });
-      }
+    // myFirstBlock ({ BOOK_NUMBER }) {
+    //     return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
+    //       .then((response) => {
+    //         if (response.ok) {
+    //           return response.json();
+    //         }
+    //         else {
+    //           return { title: 'Unknown' };
+    //         }
+    //       })
+    //       .then((bookinfo) => {
+    //         return bookinfo.title;
+    //       });
+    //   }
 
       mySecondBlock ({ MY_TEXT }) {
-        return this.syllable(MY_TEXT);
+        return MY_TEXT;
       }
 }
 
